abd6f31704609663f9f5d0d2cd6ab1830fab708c
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-06-08 11:59:13 +0200

    [test] Fix crashes in Dev Overlay Stories

--- packages/next/src/next-devtools/dev-overlay/components/hydration-diff/diff-view.tsx
+++ packages/next/src/next-devtools/dev-overlay/components/hydration-diff/diff-view.tsx
@@ -57,7 +57,7 @@ export function PseudoHtmlDiff({
 
   const htmlComponents = useMemo(() => {
     const componentStacks: React.ReactNode[] = []
-    const reactComponentDiffLines = reactOutputComponentDiff!.split('\n')
+    const reactComponentDiffLines = reactOutputComponentDiff.split('\n')
     reactComponentDiffLines.forEach((line, index) => {
       const isDiffLine = line[0] === '+' || line[0] === '-'
       const isHighlightedLine = line[0] === '>'
